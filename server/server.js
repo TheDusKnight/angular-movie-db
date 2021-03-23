e92bccb4839d3a179eb5b36ea3e66d7e71653790
TheDusKnight <860956020@qq.com>
2021-03-23 16:52:38 -0700

    03/23 begin

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 const bodyParser = require('body-parser');
 const path = require('path');
 const express = require('express');
+const axios = require('axios').default;
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -9,6 +10,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello, papa gog').end();
+});
+const url = 'https://api.themoviedb.org/3/search/multi?api_key=97588ddc4a26e3091152aa0c9a40de22&language=en-US&query=game';
+app.get('/test', (req, res) => {
+  axios.get(url).then((response) => {
+    // console.log(response)
+    res.json(response.data);
+  }).catch((error) => {
+    console.log(error);
+  });
 });
 app.get('/submit', (req, res) => {
   res.sendFile(path.join(__dirname, '/views/form.html'));

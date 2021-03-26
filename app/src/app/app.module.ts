6a2bd434ec8b9c23cc9001ba3e9b31f7f213bdd6
TheDusKnight <860956020@qq.com>
2021-03-25 18:37:42 -0700

    bootstrap probelm

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,16 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarouselPauseComponent } from './carousel-pause/carousel-pause.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarouselPauseComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

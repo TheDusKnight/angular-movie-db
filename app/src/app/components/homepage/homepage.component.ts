5a8317bbfc7dcaa7f803ce92554e399e85a9b2e4
TheDusKnight <860956020@qq.com>
2021-03-29 19:44:12 -0700

    breakpoint service worked

--- app/src/app/components/homepage/homepage.component.ts
+++ app/src/app/components/homepage/homepage.component.ts
@@ -1,20 +1,21 @@
-import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BreakpointService } from '../../services/breakpoint.service';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
-export class HomepageComponent implements OnInit, OnChanges {
+export class HomepageComponent implements OnInit {
 
   public message: string = "Passing the message from homepage";
+  public device: string;
 
-  constructor() { }
-  ngOnChanges(changes: SimpleChanges): void {
-    // throw new Error('Method not implemented.');
-  }
+  constructor(private breakpointService: BreakpointService) { }
 
   ngOnInit(): void {
+    // this.breakpoint.currentMessage.subscribe(breakpoint => this.device = breakpoint);
+    this.breakpointService.getBreakpoint().subscribe(result => this.device = result);
   }
 
 }
--- app/src/app/services/breakpoint.service.ts
+++ app/src/app/services/breakpoint.service.ts
@@ -1,12 +1,14 @@
 import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 import { Injectable } from '@angular/core';
-import { of } from 'rxjs';
-
+// import { BehaviorSubject, of } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class BreakpointService {
-  public breakpoint: string = ''
+  // public breakpoint: string = ''
+  private messageSource = new BehaviorSubject<string>('default message!!!');
+  currentMessage = this.messageSource.asObservable();
 
   constructor(public breakpointObserver: BreakpointObserver) { }
 
@@ -21,28 +23,32 @@ export class BreakpointService {
       if (state.breakpoints[Breakpoints.XSmall]) {
         console.log('Matches XSmall viewport');
         //  this.htmlStyles = "dummy1";
-        this.breakpoint = "XSmall";
+        // this.breakpoint = "XSmall";
+        this.messageSource.next('XSmall');
       }
       if (state.breakpoints[Breakpoints.Small]) {
         console.log('Matches Small viewport');
-        this.breakpoint = "Small";
+        // this.breakpoint = "Small";
+        this.messageSource.next('Small');
       }
       if (state.breakpoints[Breakpoints.Medium]) {
         console.log('Matches Medium  viewport');
-        this.breakpoint = "Medium";
+        // this.breakpoint = "Medium";
+        this.messageSource.next('Medium');
       }
       if (state.breakpoints[Breakpoints.Large]) {
 
         console.log('Matches Large viewport');
-        this.breakpoint = "Matches";
+        // this.breakpoint = "Matches";
+        this.messageSource.next('Large');
       }
       if (state.breakpoints[Breakpoints.XLarge]) {
 
         console.log('Matches XLarge viewport');
-        this.breakpoint = "XLarge";
+        this.messageSource.next('XLarge');
       }
     })
-    return of(this.breakpoint);
+    return this.currentMessage;
   }
 
 
